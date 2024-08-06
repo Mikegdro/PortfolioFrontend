1457bc0d6e02b15cd149563a354aba19fc75591d
mikegdro <miguelgdro@gmail.com>
2024-08-06 18:03:09 +0200

    Vue added for implementing Astro Islands and leveraging this for static site, Dynamic routing for Experience & Projects

---
+++ src/components/structural/Tecnologies.vue
@@ -0,0 +1,11 @@
+
+<template>
+    <h1 class="text-3xl">Tecnologies</h1>
+    <button @click="count++">Count = {{ count }}</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0)
+</script>
